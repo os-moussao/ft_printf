965725de34efe95649f50e59733f25c7e49c9145
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-20 17:08:15 +0100

    mapping

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,29 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 15:54:39 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/20 15:58:52 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/20 17:07:58 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ *
+ * todo:
+ * 	1-
+ * 		understand variadic functions
+ * 		understand these flags: - 0 . # +  space
+ * 		understand width
+ * 		make a simple version of printf that handle flags and width
+ *	
+ *	2-
+ * 		test test test: detect special cases, combinations, and errors
+ * 		fix code
+ * 		redo
+ *
+ *
+ * 		- : left justify
+ *		+ : display + sign when the number is positive
+ *		space : display hidden sign when the number is positive
+ *		0 : pad with leading zeros
+ *		. : precision
+ *		# : ?
+ **/
