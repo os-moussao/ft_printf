43b37825a8f0f54687f579e071a6b636725b1a66
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-22 16:46:12 +0100

    added more macros

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 15:43:00 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/21 17:55:05 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/22 16:11:23 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
+
+# define LL long long
+# define ULL unsigned long long
+# define LHEX "0123456789abcdef"
+# define UHEX "0123456789ABCDEF"
+# define HEX_RADIX 16
 
 #endif
