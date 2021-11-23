144f92fdfda7f766b193d2b175badabaf13aee20
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-23 13:02:11 +0100

    added string structure

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 15:43:00 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/23 00:29:19 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/23 13:01:46 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,24 @@
 # define MIN_WIDTH 'w'
 # define PRECISION 'p'
 
+typedef struct s_string
+{
+	char	*str;
+	int		len;
+}				t_string;
+
 typedef struct s_style
 {
-	char	specifier;
-	char	*varg;
-	int		min_width; // width feild
-	int		precision; // precision
-	int		lenght; // final length inc. width ans str/num length
-	char	leading_char; // spaces or zeros
-	char	sign; // ' ' or  '+' or '-'
-	bool	left_justify; // the minus flag
-	bool	hash; // 0 or 1
+	t_string	sarg;
+	char		specifier;
+	int			min_width; // width feild
+	int			precision; // precision
+	char		leading_char; // spaces or zeros
+	char		sign; // ' ' or  '+' or '-'
+	bool		left_justify; // the minus flag
+	bool		hash; // 0 or 1
 }				t_style;
+
 
 # define LL long long
 # define ULL unsigned long long
