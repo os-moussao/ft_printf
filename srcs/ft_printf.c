16b399e937cecf7a13b14a216c9023d2e13acd2e
os-moussao <78625018+os-moussao@users.noreply.github.com>
2021-11-25 15:18:24 +0100

    removed todo comment

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,47 +6,11 @@
 /*   By: omoussao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 15:54:39 by omoussao          #+#    #+#             */
-/*   Updated: 2021/11/23 18:20:55 by omoussao         ###   ########.fr       */
+/*   Updated: 2021/11/25 15:13:13 by omoussao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-/**
- * todo:
- * 	1-
- * 		understand variadic functions
- * 		understand these flags: - 0 . # +  space
- * 		understand width
- * 		make a simple version of printf that handle flags and width
- *	
- *	2-
- * 		test test test: detect special cases, combinations, and errors
- * 		fix code
- * 		redo
- *
- *	3- more stuff:
- *		take care of  printig negative hex values
- *
- *
- * 		- : left justify
- *		+ : display + sign when the number is positive
- *		space : display hidden sign when the number is positive
- *		0 : pad with leading zeros
- *		. : precision / max-width + leading zeros
- *		# : add 0x prefix in hex (%#x/%#X)
- *
- *		combinations of "0-.":
- *			flag '0' is ignored when flag '-' is present
- *
- *
- *		+ > space
- *		width > precision (if precision (max) is less than width (min))
- *
- *
- *		don't forget to handle unknown specifier --> 2 cases: random char - empthy str
- **/
-
 
 int	arg_print(t_style style, va_list ap)
 {
@@ -63,11 +27,6 @@ int	arg_print(t_style style, va_list ap)
 			return (-1);
 		ret += tmp;
 	}
-	/*
-	else if (sp == 'd' || sp == 'i')
-		ret += dec_print(style, ap);
-	...
-	*/
 	return (ret);
 }
 
